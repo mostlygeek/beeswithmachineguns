70ec5852b6ebc5290bc6dca695474096ae28fe8a
Jim Abramson <jim.abramson@magnetic.is>
2012-10-19 09:56:51 -0400

    version -> 0.6.1.10, install siege_graph script

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ from setuptools import setup, find_packages
 from multiprocessing import util
 
 setup(name='beeswithmachineguns',
-      version='0.1.6.9',
+      version='0.1.6.10',
       description='A utility for arming (creating) many bees (micro EC2 instances) to attack (load test) targets (web applications).',
       author='Christopher Groskopf',
       author_email='cgroskopf@tribune.com',
       url='http://github.com/Magentic/python-libs',
       license='MIT',
       packages=['beeswithmachineguns'],
-      scripts=['bees', 'siege_calc'],
+      scripts=['bees', 'siege_calc', 'siege_graph'],
       setup_requires=['nose'],
       test_suite='nose.collector',
       install_requires=[
