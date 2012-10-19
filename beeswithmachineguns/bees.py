bc2aa9ea91bd1e78b2fcd1912c4413dc5d1783cd
Jim Abramson <jim.abramson@magnetic.is>
2012-10-19 16:16:04 -0400

    configure siege for keep-alive (despite the "TRIPLE-CAUTION")

--- beeswithmachineguns/bees.py
+++ beeswithmachineguns/bees.py
@@ -116,6 +116,7 @@ sed 's,enabled *= *0,enabled=1,' /etc/yum.repos.d/epel.repo -i
 yum -y install gcc httpd-tools siege 
 curl https://raw.github.com/pypa/pip/master/contrib/get-pip.py | python
 pip install -qU --extra-index-url http://packages.mgnt.cc/pylibs beeswithmachineguns
+echo "connection = keep-alive" > /home/%s/.siegerc
 touch /home/%s/ready
         """ % username,
         placement=zone)
