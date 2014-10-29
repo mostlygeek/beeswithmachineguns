bb70dd20bc166890950f00bb82af12e70dff9f3a
Benson Wong <mostlygeek+git@gmail.com>
2014-10-29 10:47:44 -0700

    max out the tcp limits for the ami

--- beeswithmachineguns/bees.py
+++ beeswithmachineguns/bees.py
@@ -110,6 +110,21 @@ set -e -x
 echo 'starting'
 apt-get --yes --quiet update
 apt-get --yes --quiet install gcc siege apache2-utils
+
+echo 'maxing out tcp/network limits'
+
+# https://gist.github.com/mostlygeek/2ebde575b309676355d9
+sysctl -w net.ipv4.ip_local_port_range='2000 65000'
+sysctl -w net.ipv4.tcp_window_scaling='1';
+sysctl -w net.ipv4.tcp_max_syn_backlog='3240000';
+sysctl -w net.core.somaxconn='65535';
+sysctl -w net.ipv4.tcp_max_tw_buckets='1440000';
+sysctl -w net.core.rmem_default='8388608';
+sysctl -w net.core.rmem_max='16777216';
+sysctl -w net.core.wmem_max='16777216';
+sysctl -w net.ipv4.tcp_rmem='4096 87380 16777216';
+sysctl -w net.ipv4.tcp_wmem='4096 65536 16777216';
+
 echo 'installing stuff'"""
 
     if siege_keepalive:
